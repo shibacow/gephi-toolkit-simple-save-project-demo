9628cf000edc9982c647285c4d177224760cc3dd
shibacow <shibacow@gmail.com>
2023-02-26 04:28:56 +0900

    save project

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ set -ue
 mvn clean install
 mvn compile
 mvn test
-#JDK_JAVA_OPTIONS="--add-opens=java.base/java.net=ALL-UNNAMED" mvn exec:java -Dexec.mainClass="org.gephi.toolkit.save_test.Main" -Dexec.cleanupDaemonThreads=false
-JDK_JAVA_OPTIONS="--add-opens=java.base/java.net=ALL-UNNAMED" mvn exec:java -Dexec.mainClass="org.gephi.toolkit.save_test.Main"  -e
+JDK_JAVA_OPTIONS="--add-opens=java.base/java.net=ALL-UNNAMED" mvn exec:java -Dexec.mainClass="org.gephi.toolkit.save_test.Main" -Dexec.cleanupDaemonThreads=false
+#JDK_JAVA_OPTIONS="--add-opens=java.base/java.net=ALL-UNNAMED" mvn exec:java -Dexec.mainClass="org.gephi.toolkit.save_test.Main"  -e
--- src/main/java/org/gephi/toolkit/save_test/SaveProject.java
+++ src/main/java/org/gephi/toolkit/save_test/SaveProject.java
@@ -6,6 +6,11 @@ import java.io.File;
 
 import org.openide.util.Lookup;
 import org.gephi.project.api.ProjectController;
+import org.gephi.graph.api.GraphModel;
+import org.gephi.project.api.Workspace;
+
+import org.gephi.graph.api.DirectedGraph;
+import org.gephi.graph.api.GraphController;
 
 import java.util.logging.Logger;
 import java.util.logging.Level;
@@ -15,12 +20,26 @@ public class SaveProject {
     public void script() {
         Logger logger = Logger.getLogger(SaveProject.class.getName());
         logger.setLevel(Level.INFO);
-        //Init a project - and therefore a workspace
         ProjectController pc = Lookup.getDefault().lookup(ProjectController.class);
         logger.info("start pc");
-        pc.newProject();
+        //pc.newProject();
+        pc.openProject(new File("外事.gephi"));
+        Workspace workspace = pc.getCurrentWorkspace();
+
+        GraphModel graphModel = Lookup.getDefault().lookup(GraphController.class).getGraphModel();
+        DirectedGraph graph = graphModel.getDirectedGraph();
+        System.out.println("Nodes: " + graph.getNodeCount());
+        System.out.println("Edges: " + graph.getEdgeCount());
+
         logger.info("save project");
-        pc.saveProject(pc.getCurrentProject(),new File("save_project.gephi"));
+
+        File save_file = new File("save_project.gephi");
+        pc.saveProject(pc.getCurrentProject(),save_file);
+        if(save_file.exists()){
+            logger.info("save_project.gephi is exists");
+        }else{
+            logger.info("save_project.gephi is not exists");
+        }
         logger.info("saved project");
     }
 }
