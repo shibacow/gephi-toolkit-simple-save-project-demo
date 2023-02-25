5cbe18e5e1732ad40fa7ef8ab8e0ca868af6107d
shibacow <shibacow@gmail.com>
2023-02-26 02:42:02 +0900

    chdata

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+set -ue
 
-#bash mvn exec:java -Dexec.mainClass="org.gephi.toolkit.demos.Main" -Dexec.cleanupDaemonThreads=false 
 mvn clean install
 mvn compile
 mvn test
-#JDK_JAVA_OPTIONS="--add-opens=java.base/java.net=ALL-UNNAMED" mvn exec:java -Dexec.mainClass="org.gephi.toolkit.demos.Main" -Dexec.cleanupDaemonThreads=false 
+#JDK_JAVA_OPTIONS="--add-opens=java.base/java.net=ALL-UNNAMED" mvn exec:java -Dexec.mainClass="org.gephi.toolkit.save_test.Main" -Dexec.cleanupDaemonThreads=false
 JDK_JAVA_OPTIONS="--add-opens=java.base/java.net=ALL-UNNAMED" mvn exec:java -Dexec.mainClass="org.gephi.toolkit.save_test.Main" 
