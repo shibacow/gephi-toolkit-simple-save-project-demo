911b3b06a7bb0e9cef021f54ee0bf8ff246c0434
shibacow <shibacow@gmail.com>
2023-02-26 02:38:07 +0900

    init

---
+++ src/main/java/org/gephi/toolkit/save_test/Main.java
@@ -0,0 +1,9 @@
+package org.gephi.toolkit.save_test;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SaveProject saveProject = new SaveProject();
+        saveProject.script();
+    }
+}
---
+++ src/main/java/org/gephi/toolkit/save_test/SaveProject.java
@@ -0,0 +1,25 @@
+/*
+ */
+package org.gephi.toolkit.save_test;
+
+import java.io.File;
+
+import org.openide.util.Lookup;
+import org.gephi.project.api.ProjectController;
+
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+public class SaveProject {
+
+    public void script() {
+        Logger logger = Logger.getLogger(SaveProject.class.getName());
+        logger.setLevel(Level.INFO);
+        //Init a project - and therefore a workspace
+        ProjectController pc = Lookup.getDefault().lookup(ProjectController.class);
+        logger.info("start pc");
+        pc.newProject();
+        logger.info("save project");
+        pc.saveProject(pc.getCurrentProject(),new File("save_project.gephi"));
+    }
+}
