4350f9a28b8398e71ab967d0082827719d93aba6
shibacow <shibacow@gmail.com>
2023-03-11 06:13:37 +0900

    ch data

--- simple_save.main.kts
+++ simple_save.main.kts
@@ -1,10 +1,9 @@
 #!/usr/bin/env kotlin
 
-@file:DependsOn("lib/gephi-toolkit-0.10.0-all.jar")
-
-//@file:DependsOn("gephi-toolkit-0.9.7-all.jar")
+@file:DependsOn("lib/gephi-toolkit-0.10.1-all.jar")
 
 /*
+simple example code to save gephi file
  */
 
 import java.io.File
@@ -15,7 +14,7 @@ import org.openide.util.Lookup
 fun main(args: Array<String>){
     val pc = Lookup.getDefault().lookup(ProjectController::class.java)
     pc.newProject()
-    val out_gephi_file = File(gephi_file);
+    val out_gephi_file = File("simple_save.gephi");
     out_gephi_file.createNewFile();
     pc.saveProject(pc.getCurrentProject(), out_gephi_file)
     pc.closeCurrentProject()
