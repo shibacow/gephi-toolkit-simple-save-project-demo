3a9d6d4bab01b9d1b7f791dc4db2a15d6dcba5c0
shibacow <shibacow@gmail.com>
2023-03-11 06:15:38 +0900

    modify line

--- run.sh
+++ run.sh
@@ -4,6 +4,4 @@ mvn clean
 mvn clean install
 mvn compile
 mvn test
-#JDK_JAVA_OPTIONS="--add-opens=java.base/java.net=ALL-UNNAMED" mvn exec:java -Dexec.mainClass="org.gephi.toolkit.save_test.Main" -Dexec.cleanupDaemonThreads=false
-
-
+JDK_JAVA_OPTIONS="--add-opens=java.base/java.net=ALL-UNNAMED" mvn exec:java -Dexec.mainClass="org.gephi.toolkit.save_test.Main" -Dexec.cleanupDaemonThreads=false
