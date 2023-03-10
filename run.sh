5acacd83e4b389e5190b175097548d82da3cd588
shibacow <shibacow@gmail.com>
2023-03-11 04:27:03 +0900

    modify file save

--- run.sh
+++ run.sh
@@ -4,5 +4,9 @@ mvn clean
 mvn clean install
 mvn compile
 mvn test
-JDK_JAVA_OPTIONS="--add-opens=java.base/java.net=ALL-UNNAMED" mvn exec:java -Dexec.mainClass="org.gephi.toolkit.save_test.Main" -Dexec.cleanupDaemonThreads=false
+#JDK_JAVA_OPTIONS="--add-opens=java.base/java.net=ALL-UNNAMED" mvn exec:java -Dexec.mainClass="org.gephi.toolkit.save_test.Main" -Dexec.cleanupDaemonThreads=false
 #JDK_JAVA_OPTIONS="--add-opens=java.base/java.net=ALL-UNNAMED" mvn exec:java -Dexec.mainClass="org.gephi.toolkit.save_test.Main"  -e
+#JDK_JAVA_OPTIONS="--add-opens=java.base/java.net=ALL-UNNAMED" mvn -Dexec.mainClass="org.gephi.toolkit.save_test.Main" -Dexec.cleanupDaemonThreads=false -D"maven.surefire.debug" test
+
+JDK_JAVA_OPTIONS="--add-opens=java.base/java.net=ALL-UNNAMED" mvn exec:java -Dexec.mainClass="org.gephi.toolkit.save_test.Main" -Dexec.cleanupDaemonThreads=false
+
--- src/main/java/org/gephi/toolkit/save_test/SaveProject.java
+++ src/main/java/org/gephi/toolkit/save_test/SaveProject.java
@@ -3,6 +3,7 @@
 package org.gephi.toolkit.save_test;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.openide.util.Lookup;
 import org.gephi.project.api.ProjectController;
@@ -23,25 +24,26 @@ public class SaveProject {
         ProjectController pc = Lookup.getDefault().lookup(ProjectController.class);
         logger.info("start pc");
         pc.newProject();
-        /*
-        //pc.openProject(new File("外事.gephi"));
+        ///pc.openProject(new File("外事.gephi")).run();
         Workspace workspace = pc.getCurrentWorkspace();
 
         GraphModel graphModel = Lookup.getDefault().lookup(GraphController.class).getGraphModel();
         DirectedGraph graph = graphModel.getDirectedGraph();
         System.out.println("Nodes: " + graph.getNodeCount());
         System.out.println("Edges: " + graph.getEdgeCount());
-
         logger.info("save project");
-        */
-
-        File save_file = new File("save_project.gephi");
-        pc.saveProject(pc.getCurrentProject(),save_file).run();
-        if(save_file.exists()){
-            logger.info("save_project.gephi is exists");
-        }else{
-            logger.info("save_project.gephi is not exists");
+        try{
+            File save_file = new File("save_project.gephi");
+            save_file.createNewFile(); //work around
+            pc.saveProject(pc.getCurrentProject(),save_file).run();
+            if(save_file.exists()){
+                logger.info("save_project.gephi is exists");
+           }else{
+                logger.info("save_project.gephi is not exists");
+            }
+            logger.info("saved project");
+        }catch(IOException err){
+            err.printStackTrace();
         }
-        logger.info("saved project");
     }
 }
